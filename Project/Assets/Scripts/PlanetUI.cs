db8f0eb79df6310385aed0ccb42b8a0bf9ce732a
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-01-20 22:33:54 +0000

    Added Planet UI which activates on click.

---
+++ Project/Assets/Scripts/PlanetUI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetUI : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject planetCircle;
+
+    private bool isUIOpen = false;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray,out hit))
+            {
+                if (hit.transform.tag == "Planet")
+                {
+                    Debug.Log("is planet");
+                    ToggleUI();
+                }
+                else
+                {
+                    Debug.Log("is not planet");
+                }
+            }
+        }
+    }
+
+    void ToggleUI()
+    {
+        if (isUIOpen == true)
+        {
+            planetCircle.SetActive(false);
+            isUIOpen = false;
+        }
+        else if (isUIOpen == false)
+        {
+            planetCircle.SetActive(true);
+            isUIOpen = true;
+        }
+    }
+
+}
