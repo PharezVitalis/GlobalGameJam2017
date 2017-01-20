967d84faa7c4f800e10ccf1410b4f06af41ebc28
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-01-20 21:37:23 +0000

    Added the UI being toggled on and off from entering play/plan mode. Allows player to enter plan mode using escape.

---
+++ Project/Assets/Scripts/PlanningUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanningUI : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject planningBar;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EnterPlanMode();
+        }
+    }
+
+    public void EnterPlayMode()
+    {
+        InfoBar(true);
+    }
+
+    public void EnterPlanMode()
+    {
+        InfoBar(false);
+    }
+
+
+    public void InfoBar(bool hidePanel)
+    {
+        if (hidePanel)
+            planningBar.SetActive(false);
+
+        else if (!hidePanel)
+            planningBar.SetActive(true);
+    }
+
+
+}
