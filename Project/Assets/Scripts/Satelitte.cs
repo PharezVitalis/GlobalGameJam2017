4d682b27fd05a2d7b16fdbf7252d23b101fc6796
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2017-01-20 21:02:09 +0000

    began implement a class for satellites

---
+++ Project/Assets/Scripts/Satelitte.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Satelitte : MonoBehaviour {
+
+    
+    private Transform beamOutLocation;
+    private Quaternion lookRot;
+    private Vector3 diff;
+
+
+    void Awake()
+    {
+        beamOutLocation = GetComponentInChildren<Transform>();
+    }
+
+    public void LookAt(Vector3 targetPos)
+    {
+         diff = targetPos - transform.position;
+        diff.Normalize();
+
+        lookRot = Quaternion.LookRotation(diff, transform.TransformDirection(Vector3.up));
+
+        transform.rotation = new Quaternion(0, 0, lookRot.z, lookRot.w);
+
+    }
+	
+}
