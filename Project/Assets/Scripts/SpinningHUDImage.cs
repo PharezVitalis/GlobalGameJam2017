f7ff420ccff5c7b37b8e50062f07cb94e40aa97d
RudiPrentice <rudiprentice@gmail.com>
2017-01-21 22:53:59 +0000

    Made planets dynamic af

---
+++ Project/Assets/Scripts/SpinningHUDImage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningHUDImage : MonoBehaviour {
+
+    private bool m_spinning = true;
+    private float m_spinSpeed;
+
+    void Start()
+    {
+        m_spinSpeed = Random.Range(-0.25f, 0.25f);
+    }
+	
+	void Update ()
+    {
+        if (m_spinning)
+        {
+            gameObject.transform.Rotate(new Vector3(0, 0, 1) * m_spinSpeed);
+        }
+	}
+}
