1d014baab2d6e1af3cc0431586e1219ef0a23156
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2017-01-21 17:04:18 +0000

    mixed channel levels, fixed satellite prefab, tweaked satellite movement scripts so that the invoke can stop automatically

--- Project/Assets/Scripts/SatelitteMovement.cs
+++ Project/Assets/Scripts/SatelitteMovement.cs
@@ -46,7 +46,8 @@ public class SatelitteMovement : FromToMovement
     {
         if (!HasTarget)
         {
-            return;
+            CancelInvoke();
+            return;            
         }
 
 
