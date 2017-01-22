72d11573de31983e560dd75362952b340992eb94
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-01-22 10:51:57 +0000

    Added in asteroid and gave it a random sprite each time the game starts.

---
+++ Project/Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSprite : MonoBehaviour {
+
+
+    [SerializeField]
+    private Sprite[] sprite = new Sprite[3];
+
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        int rnd = Random.Range(0, 3);
+        sr.sprite = sprite[rnd];
+    }
+}
