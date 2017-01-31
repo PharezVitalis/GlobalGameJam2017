19cecc28d963e0715e4d65bf22b19eb6395e6654
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2017-01-31 12:31:35 +0000

    Added fade out and skip to intro

--- Project/Assets/Scripts/Intro.cs
+++ Project/Assets/Scripts/Intro.cs
@@ -7,6 +7,10 @@ public class Intro : MonoBehaviour {
     private AudioSource ambient;
     [SerializeField]
     private AudioSource launch;
+
+    [SerializeField]
+    private float timeoutFromPlay = 2;
+    private float timeRemaing;
 	
 	void Start()
     {
@@ -16,12 +20,35 @@ public class Intro : MonoBehaviour {
 
     private IEnumerator PlayLaunchSound()
     {
+        timeRemaing = timeoutFromPlay;
+
         yield return new WaitForSeconds(1.3f);
 
         launch.Play();
 
-        yield return new WaitForSeconds(2f);
+        while (timeRemaing > 0)
+        {
+            timeRemaing -= (Time.fixedDeltaTime / timeoutFromPlay);
+
+            if (timeRemaing < 1.4f)
+            {
+                launch.volume = timeRemaing / timeoutFromPlay;
+                ambient.volume = timeRemaing / timeoutFromPlay;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
 
         Application.LoadLevel(1);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            StopAllCoroutines();
+
+            Application.LoadLevel(1);
+        }
     }
 }
